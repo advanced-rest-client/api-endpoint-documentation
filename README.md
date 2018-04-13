e671907fb1c1f3d8e3b87754804def5db60c6aa7
Pawel Psztyc <jarrodek@gmail.com>
2018-04-13 16:14:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+[![Build Status](https://travis-ci.org/advanced-rest-client/api-url-data-model.svg?branch=stage)](https://travis-ci.org/advanced-rest-client/api-endpoint-documentation)
+
+[![Published on webcomponents.org](https://img.shields.io/badge/webcomponents.org-published-blue.svg)](https://www.webcomponents.org/element/advanced-rest-client/api-endpoint-documentation)
+
+# api-endpoint-documentation
+
+A component to generate documentation for a resource from AMF model
+
+<!---
+```
+<custom-element-demo>
+  <template>
+    <link rel="import" href="api-endpoint-documentation.html">
+    <next-code-block></next-code-block>
+  </template>
+</custom-element-demo>
+```
+-->
+
+```html
+<api-endpoint-documentation></api-endpoint-documentation>
+```
+
+### API components
+
+This components is a part of API components ecosystem: https://elements.advancedrestclient.com/
---
+++ test/amf-loader.js
@@ -0,0 +1,26 @@
+const AmfLoader = {};
+AmfLoader.load = function(endpointIndex) {
+  endpointIndex = endpointIndex || 0;
+  const url = location.protocol + '//' + location.host +
+    location.pathname.substr(0, location.pathname.lastIndexOf('/'))
+    .replace('/test', '/demo') + '/demo-api.json';
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', (e) => {
+      let data;
+      try {
+        data = JSON.parse(e.target.response);
+      } catch (e) {
+        reject(e);
+        return;
+      }
+      const def = data[0]['http://raml.org/vocabularies/document#encodes'][0];
+      const endpoint = def['http://raml.org/vocabularies/http#endpoint'][endpointIndex];
+      resolve([data, endpoint]);
+    });
+    xhr.addEventListener('error',
+      () => reject(new Error('Unable to load model file')));
+    xhr.open('GET', url);
+    xhr.send();
+  });
+};
