a4b5e7eb3c67918bbd8c0b775f29c49785e223f6
Pawel Psztyc <jarrodek@gmail.com>
2019-12-02 18:08:23 -0800

    chore: adding ie11 demo

---
+++ demo/server/app.js
@@ -0,0 +1,39 @@
+// import Koa from 'koa';
+import { createConfig, startServer } from 'es-dev-server';
+import fs from 'fs-extra';
+import path from 'path';
+
+async function proxy(ctx, next) {
+  if (ctx.request.url === '/demo/vendor.js') {
+    /* eslint-disable require-atomic-updates */
+    ctx.body = await fs.readFile(path.join(__dirname, '..', 'vendor.js'));
+    return;
+  }
+  if (ctx.request.url === '/node_modules/marked/lib/marked.js') {
+    const file = path.join(__dirname, '..', '..', 'node_modules', 'marked', 'lib', 'marked.js');
+    const body = await fs.readFile(file, 'utf8');
+    ctx.response.set('content-type', 'text/javascript');
+    ctx.body = body.replace('(this || (', '(window || (');
+    return;
+  }
+  return next();
+}
+
+const config = createConfig({
+  // port: 8001,
+  watch: true,
+  nodeResolve: true,
+  appIndex: 'demo/index.html',
+  moduleDirs: ['node_modules'],
+  logStartup: true,
+  openBrowser: true,
+  compatibility: 'auto',
+  babelExclude: [
+    '**/vendor.js'
+  ],
+  middlewares: [
+    proxy,
+  ]
+});
+config.openBrowser = true;
+startServer(config);
