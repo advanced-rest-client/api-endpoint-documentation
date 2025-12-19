04442e2957efdb45d7477b89db11f5b03b56b0bb
Alex Perez <alexperez@mulesoft.com>
2025-12-19 12:25:17 -0300

    refactor: simplify web-test-runner configuration
    - Removed TypeScript type annotations and unnecessary HTML template from the web-test-runner configuration file for improved readability.

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,26 +1,13 @@
-/** @typedef {import('@web/test-runner').TestRunnerConfig} TestRunnerConfig */
-
-export default /** @type TestRunnerConfig */ ({
-  files: 'test/**/*.test.js',
-  nodeResolve: true,
-  middleware: [
-    function rewriteBase(context, next) {
-      if (context.url.indexOf('/base') === 0) {
-        context.url = context.url.replace('/base', '');
-      }
-      return next();
-    }
-  ],
-  testFramework: {
-    config: {
-      timeout: 10000,
-    },
-  },
-  testRunnerHtml: (testFramework) =>
-    `<html>
-		<body>
-		  <script src="../demo/vendor.js"></script>
-		  <script type="module" src="${testFramework}"></script>
-		</body>
-	  </html>`
-});
+export default {
+	files: 'test/**/*.test.js',
+	nodeResolve: true,
+	middleware: [
+	  function rewriteBase(context, next) {
+		if (context.url.indexOf('/base') === 0) {
+		  context.url = context.url.replace('/base', '');
+		}
+		return next();
+	  }
+	],
+  };
+  
